6e035a79ccef644521fb9897e6898d732126196e
Alik532UA <Alik532UA@users.noreply.github.com>
2026-09-23 08:31:58 +0000

    deploy: a8b0b124570216fd0d4924120ec7831adee71c39

--- _app/immutable/chunks/DHzmnXXX.js
+++
@@ -1,2 +0,0 @@
-import{s as i,g as o,a}from"./DTy_pSb5.js";import{r as n,w as r}from"./C97bAg1r.js";const w=["both","main","log"],t="view.info",c=s=>w.includes(s);class m{#e=i("both");get view(){return o(this.#e)}set view(e){a(this.#e,e,!0)}init(){const e=n(t);c(e)&&(this.view=e)}set(e){this.view=e,r(t,e)}}const V=new m;export{w as V,V as s};
-//# sourceMappingURL=DHzmnXXX.js.map
